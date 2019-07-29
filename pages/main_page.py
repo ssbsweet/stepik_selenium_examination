309382b4da9557d0dac81033eee0e9e31bacacfd
ssbsweet <ssbsweet56@gmail.com>
2019-07-29 17:53:11 +0300

    another_yet_update

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,7 +1,7 @@
 from .base_page import BasePage # импорт базового класса BasePage из файла basepage.py
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):   # этот класс наследник basepage
-    def go_to_login_page(self):
-        login_link = self.browser.find_element_by_css_selector("#login_link")
-        login_link.click()
+    def should_be_login_link(self):
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
